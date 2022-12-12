17aedf658c13102913abb75ce7874345dea18c5d
赵鹏 <18303881822@163.com>
2022-12-12 20:55:51 +0800

    first commit

---
+++ assets/index.69f0dd90.js
@@ -0,0 +1,1 @@
+import{d as a,f as r,u as s,r as c,o as t}from"./index.34df28db.js";const p=a({__name:"index",setup(_){let o=e=>{console.log(e)};return(e,l)=>{const n=c("m-choose-area");return t(),r(n,{onChange:s(o)},null,8,["onChange"])}}});export{p as default};
---
+++ assets/index.bcb5f9a4.js
@@ -0,0 +1,1 @@
+import{_ as e,f as c,o as n,r as t}from"./index.34df28db.js";const s={};function r(_,a){const o=t("m-choose-city");return n(),c(o)}const m=e(s,[["render",r]]);export{m as default};
